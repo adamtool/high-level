f78fb498ff06d02a48f6edba8f574a164259882c
Lukas Panneke <l.panneke@googlemail.com>
2020-08-20 21:47:05 +0200

    Factor out some common rendering behaviour

--- src/uniolunisaar/adam/pnml/PnmlRenderer.java
+++ src/uniolunisaar/adam/pnml/PnmlRenderer.java
@@ -16,6 +16,7 @@ import uniolunisaar.adam.ds.highlevel.ColorDomain;
 import uniolunisaar.adam.ds.highlevel.ColorToken;
 import uniolunisaar.adam.ds.highlevel.ColorTokens;
 import uniolunisaar.adam.ds.highlevel.HLPetriGame;
+import uniolunisaar.adam.ds.highlevel.StaticColorClass;
 import uniolunisaar.adam.ds.highlevel.arcexpressions.ArcExpression;
 import uniolunisaar.adam.ds.highlevel.arcexpressions.ArcTuple;
 import uniolunisaar.adam.ds.highlevel.arcexpressions.IArcTerm;
@@ -95,8 +96,6 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 		}
 
 		private Element renderDeclaration() {
-			Element declaration = dom.createElement("declaration");
-			Element structure = dom.createElement("structure");
 			Element declarations = dom.createElement("declarations");
 
 			for (BasicColorClass cc : game.getBasicColorClasses()) {
@@ -119,22 +118,20 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 
 			renderVariableDeclaration().forEach(declarations::appendChild);
 
-			structure.appendChild(declarations);
-			declaration.appendChild(structure);
-			return declaration;
+			return wrapIn("declaration", wrapIn("structure", declarations));
 		}
 
 		private Element renderPartition(BasicColorClass cc) {
 			Element partition = dom.createElement("partition");
 			partition.setAttribute("id", cc.getId() + "Partition");
-			partition.appendChild(renderUsersortReference(cc.getId()));
+			partition.appendChild(renderColorClassReference(cc));
 
 			cc.getStaticSubclasses().forEach((id, subclass) -> {
 				Element partitionelement = dom.createElement("partitionelement");
 				partitionelement.setAttribute("id", id);
 
 				for (Color color : subclass.getColors()) {
-					partitionelement.appendChild(renderUseroperatorReference(color.getId()));
+					partitionelement.appendChild(renderColorReference(color));
 				}
 
 				partition.appendChild(partitionelement);
@@ -167,7 +164,7 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 
 			Element productsort = dom.createElement("productsort");
 			for (String factor : domain) {
-				productsort.appendChild(renderUsersortReference(factor));
+				productsort.appendChild(renderColorClassReference(factor));
 			}
 
 			namedsort.appendChild(productsort);
@@ -260,7 +257,7 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 					.map(nameTypePair -> {
 						Element variabledecl = dom.createElement("variabledecl");
 						variabledecl.setAttribute("id", idChooser.apply(nameTypePair));
-						variabledecl.appendChild(renderUsersortReference(nameTypePair.getSecond()));
+						variabledecl.appendChild(renderColorClassReference(nameTypePair.getSecond()));
 						return variabledecl;
 					})
 					.collect(Collectors.toList());
@@ -334,16 +331,10 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 		}
 
 		private Element renderTypeReference(ColorDomain domain) {
-			Element type = dom.createElement("type");
-			Element structure = dom.createElement("structure");
-			structure.appendChild(renderUsersortReference(tupleName(domain)));
-
-			type.appendChild(structure);
-			return type;
+			return wrapIn("type", wrapIn("structure", renderColorClassReference(tupleName(domain))));
 		}
 
 		private Element renderInitialMarking(ColorTokens tokens) {
-			Element hlinitialMarking = dom.createElement("hlinitialMarking");
 			Element structure = dom.createElement("structure");
 
 			if (tokens.size() == 1) {
@@ -359,41 +350,26 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 				 */
 				Element add = dom.createElement("add");
 				for (ColorToken token : tokens) {
-					Element subterm = dom.createElement("subterm");
-					subterm.appendChild(renderColorToken(token));
-					add.appendChild(subterm);
+					add.appendChild(wrapInSubterm(renderColorToken(token)));
 				}
 				structure.appendChild(add);
 			}
 
-			hlinitialMarking.appendChild(structure);
-			return hlinitialMarking;
+			return wrapIn("hlinitialMarking", structure);
 		}
 
 		private Element renderColorToken(ColorToken token) {
-			Element numberof = dom.createElement("numberof");
-			Element multiplicitySubterm = dom.createElement("subterm");
-			multiplicitySubterm.appendChild(renderNumberconstant(1));
-
-			Element typeSubterm = dom.createElement("subterm");
 			if (token.size() == 1) {
-				typeSubterm.appendChild(renderUseroperatorReference(token.get(0).getId()));
+				return wrapInNumberof(renderColorReference(token.get(0)));
 			} else {
-				typeSubterm.appendChild(renderTuple(token));
+				return wrapInNumberof(renderTuple(token));
 			}
-
-			numberof.appendChild(multiplicitySubterm);
-			numberof.appendChild(typeSubterm);
-			return numberof;
 		}
 
 		private Element renderTuple(ColorToken token) {
 			Element tuple = dom.createElement("tuple");
 			for (int i = 0; i < token.size(); i++) {
-				Element subterm = dom.createElement("subterm");
-				subterm.appendChild(renderUseroperatorReference(token.get(i).getId()));
-
-				tuple.appendChild(subterm);
+				tuple.appendChild(wrapInSubterm(renderColorReference(token.get(i))));
 			}
 			return tuple;
 		}
@@ -421,39 +397,31 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 			Element condition = dom.createElement("condition");
 			Element text = dom.createElement("text");
 			text.setTextContent(predicate.toString());
-			Element structure = dom.createElement("structure");
-			structure.appendChild(renderPredicate(predicate));
-
 			condition.appendChild(text);
-			condition.appendChild(structure);
+			condition.appendChild(wrapIn("structure", renderPredicate(predicate)));
 			return condition;
 		}
 
 		private Element renderPredicate(IPredicate predicate) {
 			if (predicate instanceof Constants) {
 				Constants constant = (Constants) predicate;
-				Element booleanconstant = dom.createElement("booleanconstant");
+				String value;
 				switch (constant) {
 					case FALSE:
-						booleanconstant.setAttribute("value", "false");
+						value = "false";
 						break;
 					case TRUE:
-						booleanconstant.setAttribute("value", "true");
+						value = "true";
 						break;
 					default:
 						throw new UnsupportedOperationException("Unknown constant: " + predicate);
 				}
-				return booleanconstant;
+				return renderElementWithAttribute("booleanconstant", "value", value);
 			} else if (predicate instanceof UnaryPredicate) {
 				UnaryPredicate unary = (UnaryPredicate) predicate;
 				switch (unary.getOperator()) {
 					case NEG:
-						Element not = dom.createElement("not");
-						Element subterm = dom.createElement("subterm");
-						subterm.appendChild(renderPredicate(unary.getOperand()));
-
-						not.appendChild(subterm);
-						return not;
+						return wrapInElementWithSubterms("not", renderPredicate(unary.getOperand()));
 					default:
 						throw new UnsupportedOperationException("Unknown unary operator " + unary.getOperator() + " in " + predicate);
 				}
@@ -476,15 +444,10 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 					default:
 						throw new UnsupportedOperationException("Unknown binary operator " + binary.getOperator() + " in " + predicate);
 				}
-				Element operator = dom.createElement(elementName);
-				Element left = dom.createElement("subterm");
-				Element right = dom.createElement("subterm");
-				left.appendChild(renderPredicate(binary.getLeftOperand()));
-				right.appendChild(renderPredicate(binary.getRightOperand()));
-
-				operator.appendChild(left);
-				operator.appendChild(right);
-				return operator;
+				return wrapInElementWithSubterms(elementName,
+						renderPredicate(binary.getLeftOperand()),
+						renderPredicate(binary.getRightOperand())
+				);
 			} else if (predicate instanceof BasicPredicate) {
 				BasicPredicate<?> equality = (BasicPredicate<?>) predicate;
 				if (equality.getLeftOperand() instanceof DomainTerm) {
@@ -498,15 +461,10 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 		}
 
 		private Element renderEquality(BasicPredicate<?> equality) {
-			Element operator = dom.createElement(equality.getOperator() == BasicPredicate.Operator.EQ ? "equality" : "inequality");
-			Element left = dom.createElement("subterm");
-			Element right = dom.createElement("subterm");
-			left.appendChild(renderPredicateTerm(equality.getLeftOperand()));
-			right.appendChild(renderPredicateTerm(equality.getRightOperand()));
-
-			operator.appendChild(left);
-			operator.appendChild(right);
-			return operator;
+			return wrapInElementWithSubterms(equality.getOperator() == BasicPredicate.Operator.EQ ? "equality" : "inequality",
+					renderPredicateTerm(equality.getLeftOperand()),
+					renderPredicateTerm(equality.getRightOperand())
+			);
 		}
 
 		private <T extends IPredicateType> Element renderPredicateTerm(IPredicateTerm<T> term) {
@@ -552,32 +510,34 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 			Variable variable = domainTerm.getVariables().iterator().next();
 			List<Element> equalities = new LinkedList<>();
 			for (Color color : getColorsOfClass(colorClassTerm.getClassId())) {
-				Element operator = dom.createElement(equality.getOperator() == BasicPredicate.Operator.EQ ? "equality" : "inequality");
-				Element left = dom.createElement("subterm");
-				Element right = dom.createElement("subterm");
-				left.appendChild(renderVariableReference(variable));
-				right.appendChild(renderUseroperatorReference(color.getId()));
-
-				operator.appendChild(left);
-				operator.appendChild(right);
-				equalities.add(operator);
+				equalities.add(wrapInElementWithSubterms(equality.getOperator() == BasicPredicate.Operator.EQ ? "equality" : "inequality",
+						renderVariableReference(variable),
+						renderColorReference(color)
+				));
 			}
 			Iterator<Element> subterms = equalities.iterator();
 			return renderNaryPredicate(equality.getOperator() == BasicPredicate.Operator.EQ ? "or" : "and", subterms.next(), subterms);
+		}
+
+		private List<Color> getColorsOfClass(String classOrSubClassId) {
+			if (game.hasStaticSubclass(classOrSubClassId)) {
+				return game.getBasicColorClassOfStaticSubclass(classOrSubClassId)
+						.getStaticSubclasses()
+						.get(classOrSubClassId)
+						.getColors();
+			} else {
+				return game.getBasicColorClass(classOrSubClassId).getColors();
+			}
 		}
 
 		private Element renderNaryPredicate(String operator, Element current, Iterator<Element> rest) {
 			if (!rest.hasNext()) {
 				return current;
 			}
-			Element operatorElement = dom.createElement(operator);
-			Element left = dom.createElement("subterm");
-			Element right = dom.createElement("subterm");
-			left.appendChild(current);
-			right.appendChild(renderNaryPredicate(operator, rest.next(), rest));
-			operatorElement.appendChild(left);
-			operatorElement.appendChild(right);
-			return operatorElement;
+			return wrapInElementWithSubterms(operator,
+					current,
+					renderNaryPredicate(operator, rest.next(), rest)
+			);
 		}
 
 		private Element renderArc(Flow flow) {
@@ -587,14 +547,7 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 			arc.setAttribute("id", source + "-" + target);
 			arc.setAttribute("source", source);
 			arc.setAttribute("target", target);
-
-			Element hlinscription = dom.createElement("hlinscription");
-			Element structure = dom.createElement("structure");
-
-			structure.appendChild(renderArcExpression(game.getArcExpression(flow)));
-
-			hlinscription.appendChild(structure);
-			arc.appendChild(hlinscription);
+			arc.appendChild(wrapIn("hlinscription", wrapIn("structure", renderArcExpression(game.getArcExpression(flow)))));
 			return arc;
 		}
 
@@ -605,29 +558,13 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 			} else if (expressions.size() > 1) {
 				Element add = dom.createElement("add");
 				for (var expression : expressions) {
-					Element subterm = dom.createElement("subterm");
-					subterm.appendChild(renderSingleArcExpressionSubterm(expression));
-
-					add.appendChild(subterm);
+					add.appendChild(wrapInSubterm(renderSingleArcExpressionSubterm(expression)));
 				}
 				return add;
 			} else /* expressions.size() == 0 */ {
 				// no expression sometimes means dot variable.
 				throw new UnsupportedOperationException("every arc must have at least one arc expression");
 			}
-		}
-
-		private Element wrapInNumberof(Element element) {
-			Element numberof = dom.createElement("numberof");
-			Element multiplicitySubterm = dom.createElement("subterm");
-			multiplicitySubterm.appendChild(renderNumberconstant(1));
-
-			Element contentSubterm = dom.createElement("subterm");
-			contentSubterm.appendChild(element);
-
-			numberof.appendChild(multiplicitySubterm);
-			numberof.appendChild(contentSubterm);
-			return numberof;
 		}
 
 		private Element renderSingleArcExpressionSubterm(Pair<IArcTerm.Sort, IArcTerm<? extends IArcType>> expression) {
@@ -686,8 +623,7 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 						break;
 					case COLORCLASS: {
 						List<Element> colors = getColorsOfClass(((ColorClassTerm) tupleElement.getSecond()).getClassId()).stream()
-								.map(Color::getId)
-								.map(this::renderUseroperatorReference)
+								.map(this::renderColorReference)
 								.collect(Collectors.toUnmodifiableList());
 						plusMinus.add(new Pair<>(
 								colors,
@@ -699,8 +635,7 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 						SetMinusTerm setMinus = (SetMinusTerm) tupleElement.getSecond();
 						plusMinus.add(new Pair<>(
 								getColorsOfClass(setMinus.getClazz().getClassId()).stream()
-										.map(Color::getId)
-										.map(this::renderUseroperatorReference)
+										.map(this::renderColorReference)
 										.collect(Collectors.toUnmodifiableList()),
 								setMinus.getVariables().stream()
 										.map(this::renderVariableReference)
@@ -763,51 +698,62 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 		}
 
 		private Element renderNeighbourTerm(Variable var, boolean predecessor) {
-			Element neighbour = dom.createElement(predecessor ? "predecessor" : "successor");
-			Element subterm = dom.createElement("subterm");
-			subterm.appendChild(renderVariableReference(var));
-
-			neighbour.appendChild(subterm);
-			return neighbour;
+			return wrapInElementWithSubterms(predecessor ? "predecessor" : "successor", renderVariableReference(var));
 		}
 
 		private Element renderColorClassTerm(ColorClassTerm term) {
-			Element all = dom.createElement("all");
-			all.appendChild(renderUsersortReference(term.getClassId()));
-			return all;
+			return wrapIn("all", renderColorClassReference(term.getClassId()));
 		}
 
 		private Element renderSetMinusTerm(SetMinusTerm term) {
 			Element subtract = dom.createElement("subtract");
-			Element setSubterm = dom.createElement("subterm");
-			setSubterm.appendChild(wrapInNumberof(renderColorClassTerm(term.getClazz())));
-			subtract.appendChild(setSubterm);
+			subtract.appendChild(wrapInSubterm(wrapInNumberof(renderColorClassTerm(term.getClazz()))));
 			for (Variable variable : term.getVariables()) {
-				Element minusSubterm = dom.createElement("subterm");
-				minusSubterm.appendChild(wrapInNumberof(renderVariableReference(variable)));
-
-				subtract.appendChild(minusSubterm);
+				subtract.appendChild(wrapInSubterm(wrapInNumberof(renderVariableReference(variable))));
 			}
 
 			return subtract;
 		}
 
-		private Element renderUseroperatorReference(String id) {
-			Element useroperator = dom.createElement("useroperator");
-			useroperator.setAttribute("declaration", id);
-			return useroperator;
-		}
-
-		private Element renderUsersortReference(String id) {
-			Element useroperator = dom.createElement("usersort");
-			useroperator.setAttribute("declaration", id);
-			return useroperator;
+		private Element renderVariableReference(String variableId) {
+			return renderElementWithAttribute("variable", "refvariable", variableId);
 		}
 
 		private Element renderVariableReference(Variable variable) {
-			Element variableElement = dom.createElement("variable");
-			variableElement.setAttribute("refvariable", variable.getName());
-			return variableElement;
+			return renderVariableReference(variable.getName());
+		}
+
+		private Element renderColorReference(String colorId) {
+			return renderElementWithAttribute("useroperator", "declaration", colorId);
+		}
+
+		private Element renderColorReference(Color color) {
+			return renderColorReference(color.getId());
+		}
+
+		private Element renderColorClassReference(String colorClassId) {
+			return renderElementWithAttribute("usersort", "declaration", colorClassId);
+		}
+
+		private Element renderColorClassReference(BasicColorClass colorClass) {
+			return renderColorClassReference(colorClass.getId());
+		}
+
+		private Element renderColorClassReference(StaticColorClass colorClass) {
+			return renderColorClassReference(colorClass.getId());
+		}
+
+		private Element renderElementWithAttribute(String tagName, String attributeName, String attributeValue) {
+			Element Element = dom.createElement(tagName);
+			Element.setAttribute(attributeName, attributeValue);
+			return Element;
+		}
+
+		private Element wrapInNumberof(Element element) {
+			return wrapInElementWithSubterms("numberof",
+					renderNumberconstant(1),
+					element
+			);
 		}
 
 		private Element renderNumberconstant(long value) {
@@ -820,27 +766,19 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 		}
 
 		private Element renderName(String value) {
-			Element name = dom.createElement("name");
 			Element text = dom.createElement("text");
 			text.setTextContent(value);
-
-			name.appendChild(text);
-			return name;
+			return wrapIn("name", text);
 		}
 
 		private Optional<Element> renderGraphics(Node node) {
 			if (!PetriNetExtensionHandler.hasXCoord(node) || !PetriNetExtensionHandler.hasYCoord(node)) {
 				return Optional.empty();
 			}
-			double x = PetriNetExtensionHandler.getXCoord(node);
-			double y = PetriNetExtensionHandler.getYCoord(node);
-			Element graphics = dom.createElement("graphics");
 			Element position = dom.createElement("position");
-			position.setAttribute("x", Double.toString(x));
-			position.setAttribute("y", Double.toString(y));
-
-			graphics.appendChild(position);
-			return Optional.of(graphics);
+			position.setAttribute("x", Double.toString(PetriNetExtensionHandler.getXCoord(node)));
+			position.setAttribute("y", Double.toString(PetriNetExtensionHandler.getYCoord(node)));
+			return Optional.of(wrapIn("graphics", position));
 		}
 
 		/* a one-tuple's name is just the only color class name */
@@ -858,15 +796,12 @@ public class PnmlRenderer extends AbstractRenderer<HLPetriGame> implements Rende
 			return wrapIn("subterm", element);
 		}
 
-		private List<Color> getColorsOfClass(String classOrSubClassId) {
-			if (game.hasStaticSubclass(classOrSubClassId)) {
-				return game.getBasicColorClassOfStaticSubclass(classOrSubClassId)
-						.getStaticSubclasses()
-						.get(classOrSubClassId)
-						.getColors();
-			} else {
-				return game.getBasicColorClass(classOrSubClassId).getColors();
+		private Element wrapInElementWithSubterms(String tagName, Element... subtermContent) {
+			Element element = dom.createElement(tagName);
+			for (Element content : subtermContent) {
+				element.appendChild(wrapInSubterm(content));
 			}
+			return element;
 		}
 
 	}
